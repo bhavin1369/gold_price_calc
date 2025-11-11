81e4d77ea70645e44beeb0571d05a4a1db94d660
bhavin1369 <muchhalabhavin@gmail.com>
2025-11-11 14:37:13 +0530

    Deploy Flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3329037421"
+    serviceWorkerVersion: "2179796452"
   }
 });
